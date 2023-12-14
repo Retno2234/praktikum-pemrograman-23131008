e596d24ca7a9026ca22790168c32155e7f7bf4a3
Retno2234 <153788111+Retno2234@users.noreply.github.com>
2023-12-14 09:48:08 +0700

    upload praktikum a1

---
+++ praktikum-a1.py
@@ -0,0 +1,93 @@
+print('Praktikum-1') 
+print('nama lengkap: Retno Helviani')
+print('NIM         : 231031008')
+print('prodi       : Sistem Informasi')
+print()
+
+x=2023
+
+print('angkatan',x) 
+print()
+a=10
+print('Data adalah:  ',a)
+print('tipe data adalah',type(a))
+print()
+
+b=10.0  
+print('tipe data a adalah',type(a))
+print('tipe data b adalah',type(b))
+print()
+
+kampus = 'Institut Teknologi BJ Habibie'
+print('Data adalah:  ',kampus)
+print('tipe data kampus adalah',type(kampus))  
+print()
+
+c=complex(2,3)
+print('Data adalah:  ',c)
+print('tipe data adalah',type(c))
+print()
+
+log = True
+print('Data adalah:  ',log)
+print('tipe data adalah',type(log))
+print()
+
+p=20
+q=23
+hasil = p + q
+print('Hasil',p,'+',q,'=',hasil)
+print('Ini adalah batas untuk tugas praktikum###')
+print()
+
+print('1. perkalian')
+a=6
+b=8
+hasil = a * b
+print('Hasil' ,a,'*',b,'=',hasil)
+
+a=6
+b=8
+c= 7
+hasil = a * b * c
+print('Hasil' ,a,'*',b,'*',c,'=',hasil)
+
+print('2. pembagian')
+a=12
+b=4
+c=2
+hasil = a / b / c
+print('Hasil' ,a,'/',b,'/',c,'=',hasil)
+
+print('3. floor division')
+a=17
+b=8
+hasil = a // b
+print('Hasil' ,a,'//',b,'=',hasil)
+
+print('4. modulus')
+a=16
+b=7
+hasil = a % b
+print('Hasil' , a,'%',b,'=',hasil)
+
+print('5. eksponensial')
+a=54
+b=4
+hasil = a ** b
+print('Hasil' ,a,'**',b,'=',hasil)
+
+print('6. pengurangan')
+a=34
+b=16
+hasil = a - b
+print('Hasil' ,a,'-',b,'=',hasil)
+
+print('7. penjumlahan')
+a=17
+b=18
+hasil = a + b
+print('Hasil' ,a,'+',b,'=',hasil)
+
+
+
